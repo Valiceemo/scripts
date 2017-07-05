ce48dc2d48ad841f96bcbd2846fecb4d59940ad5
Valiceemo <richiewallace83@gmail.com>
2017-07-05 16:26:41 +0100

    Update external-ip-check.sh

--- system/external-ip-check.sh
+++ system/external-ip-check.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
+
 # Checks external IP and send a mail to root if updated
 
-## email address
-source /home/pi/scripts/system/source-vars/email.conf
 
-# First we get last known external IP
-LastKnownIP=$(tail -1 /var/log/external-ip.log| awk '{print $2}')
-
-# Then we need our current external IP
+LastIP=$(tail -1 /var/log/external-ip.log| awk '{print $2}')
 CurrentIP=$(curl -s https://api.ipify.org)
 
-# Do we got a new IP? If so, log it and inform root
-if [[ "$CurrentIP" != "$LastKnownIP" ]]
+if [[ "$CurrentIP" != "$LastIP" ]]
 then
-        date +"%Y-%m-%d: " | tr -d '\n' >> /var/log/external-ip.log
-        echo "$CurrentIP"  >> /var/log/external-ip.log
-        echo "`hostname` has a new external IP, $CurrentIP"  |mail -s "`hostname` IP Address Updated" "$EMAIL"
+date +"%Y-%m-%d: " | tr -d '\n' >> /var/log/external-ip.log
+echo "$CurrentIP"  >> /var/log/external-ip.log
+echo "`hostname` has a new external IP, $CurrentIP" | mail -s "`hostname` IP Address Updated" "$EMAIL"
 fi
