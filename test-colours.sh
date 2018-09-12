f506ad3ace812ab4ff59d227d36b0a005e9ffc2c
Valiceemo <richiewallace83@gmail.com>
2018-09-12 13:46:03 +0000

    Update test-colours.sh

--- test-colours.sh
+++ test-colours.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
-COL_GREEN=' '
-COL_NC=" "
+# COLORS
+blackText=$(tput setaf 0)   # Black
+redText=$(tput setaf 1)     # Red
+greenText=$(tput setaf 2)   # Green
+yellowText=$(tput setaf 3)  # Yellow
+blueText=$(tput setaf 4)    # Blue
+magentaText=$(tput setaf 5) # Magenta
+cyanText=$(tput setaf 6)    # Cyan
+whiteText=$(tput setaf 7)   # White
+resetText=$(tput sgr0)      # Reset to default color
 
-
-TICK="[${COL_GREEN}OK${COL_NC}]"
+TICK="[${greenText}✓${resetText}]"
 INFO="[i]"
-DONE="${COL_GREEN}  done!${COL_NC}"
+DONE="${greenText}  done!${resetText}"
 
 str="Consolidating blocklists"
 echo -ne "  ${INFO} ${str}..."
+str="All done here big man"
+echo "  ${TICK} ${str}
