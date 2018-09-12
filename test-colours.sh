a0283ad3f0ca497e77e81e27754fb3ec24b05a03
Valiceemo <richiewallace83@gmail.com>
2018-09-12 13:40:22 +0000

    Create test-colours.sh

---
+++ test-colours.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+COL_GREEN=' '
+COL_NC=" "
+
+
+TICK="[${COL_GREEN}OK${COL_NC}]"
+INFO="[i]"
+DONE="${COL_GREEN}  done!${COL_NC}"
+
+str="Consolidating blocklists"
+echo -ne "  ${INFO} ${str}..."
